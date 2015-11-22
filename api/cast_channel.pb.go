3f340ec96a20ed7c7cf82056c08ae3d46d77d305
Phil Sorber <sorber@apache.org>
2015-11-22 13:52:27 -0700

    Change imoprts from code.google.com to github.com

--- api/cast_channel.pb.go
+++ api/cast_channel.pb.go
@@ -17,7 +17,7 @@ It has these top-level messages:
 */
 package api
 
-import proto "code.google.com/p/gogoprotobuf/proto"
+import proto "github.com/gogo/protobuf/proto"
 import json "encoding/json"
 import math "math"
 
